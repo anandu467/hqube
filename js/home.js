76fd51be58d5e4e14795547e209fdb012876a4b4
Anandu M <you@example.com>
2021-07-25 10:04:28 +0530

    assets added

---
+++ js/home.js
@@ -0,0 +1,19 @@
+let button = document.querySelector('#dark-toggle');
+
+button.addEventListener('click', ()=>{
+
+  document.documentElement.classList.toggle('dark-mode');
+
+
+  excluded=[".contact_btn",".contact_btn-main"]
+
+  excluded.forEach((element)=>{
+
+  document.querySelector(element).classList.toggle('undo-invert');
+
+  });
+  document.querySelector('#dark-toggle>i').classList.toggle("fa-moon");
+  document.querySelector('#dark-toggle>i').classList.toggle("fa-lightbulb");
+
+  
+});
