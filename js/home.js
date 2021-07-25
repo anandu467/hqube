a4f415f0e8f2ecd3e7465e1e929457be86487020
Anandu M <you@example.com>
2021-07-26 00:23:00 +0530

    Services section added

--- js/home.js
+++ js/home.js
@@ -17,3 +17,24 @@ button.addEventListener('click', ()=>{
 
   
 });
+
+function setAnimation(file) {
+
+console.log(file);
+  document.querySelector("#animation-image").src = file;
+  
+}
+
+
+service1=document.querySelector("#service1");
+service2=document.querySelector("#service2");
+service3=document.querySelector("#service3");
+service4=document.querySelector("#service4");
+
+
+
+service1.addEventListener("mouseover",()=>{setAnimation("./assets/images/web_design.gif")});
+service2.addEventListener("mouseover",()=>{setAnimation("./assets/images/app_design.gif")});
+service3.addEventListener("mouseover",()=>{setAnimation("./assets/images/marketing.gif")});
+service4.addEventListener("mouseover",()=>{setAnimation("./assets/images/ui_design.gif")});
+
